e2bbc5631bb8fbc06c3b465a291e0a1b75f48f38
Bazanova-Anzelika-Sergeevna2782002 <148009844+Bazanova-Anzelika-Sergeevna2782002@users.noreply.github.com>
2023-10-15 23:26:10 +0300

    Add files via upload

---
+++ Лабораторная работа1/task2.py
@@ -0,0 +1,10 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+kol_vo = len(list_players)
+middle_index = kol_vo // 2
+
+left_team = list_players[:middle_index]
+right_team = list_players[middle_index:]
+
+print(left_team)
+print(right_team)
+# TODO Разделите участников на две команды
